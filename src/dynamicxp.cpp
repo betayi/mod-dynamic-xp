91784edf4cd4cddc463ea18d556755473e22f9c8
Beta.Yi <beta.yi@qq.com>
2022-12-14 11:52:25 +0800

    Update dynamicxp.cpp

--- src/dynamicxp.cpp
+++ src/dynamicxp.cpp
@@ -17,7 +17,7 @@ class spp_dynamic_xp_rate : public PlayerScript
 
         void OnLogin(Player* player) override
        {
-            if (sConfigMgr->GetBoolDefault("Dynamic.XP.Rate.Announce", true))
+            if (sConfigMgr->GetOption<bool>("Dynamic.XP.Rate.Announce", true))
                {
                ChatHandler(player->GetSession()).SendSysMessage("This server is running the |cff4CFF00Level Dynamic XP |rmodule.");
                }
@@ -29,28 +29,28 @@ class spp_dynamic_xp_rate : public PlayerScript
         
 	   {
            if (player->getLevel() <= 9)
-            amount *= sConfigMgr->GetIntDefault("Dynamic.XP.Rate.1-9", 1);
+            amount *= sConfigMgr->GetOption<int32>("Dynamic.XP.Rate.1-9", 1);
 
            else if (player->getLevel() <= 19)
-            amount *= sConfigMgr->GetIntDefault("Dynamic.XP.Rate.10-19", 2);
+            amount *= sConfigMgr->GetOption<int32>("Dynamic.XP.Rate.10-19", 2);
 
            else if (player->getLevel() <= 29)
-            amount *= sConfigMgr->GetIntDefault("Dynamic.XP.Rate.20-29", 3);
+            amount *= sConfigMgr->GetOption<int32>("Dynamic.XP.Rate.20-29", 3);
 
            else if (player->getLevel() <= 39)
-            amount *= sConfigMgr->GetIntDefault("Dynamic.XP.Rate.30-39", 4);
+            amount *= sConfigMgr->GetOption<int32>("Dynamic.XP.Rate.30-39", 4);
 
            else if (player->getLevel() <= 49)
-            amount *= sConfigMgr->GetIntDefault("Dynamic.XP.Rate.40-49", 5);
+            amount *= sConfigMgr->GetOption<int32>("Dynamic.XP.Rate.40-49", 5);
 
            else if (player->getLevel() <= 57)
-            amount *= sConfigMgr->GetIntDefault("Dynamic.XP.Rate.50-57", 6);
+            amount *= sConfigMgr->GetOption<int32>("Dynamic.XP.Rate.50-57", 6);
 
            else if (player->getLevel() <= 67)
-            amount *= sConfigMgr->GetIntDefault("Dynamic.XP.Rate.60-67", 7);
+            amount *= sConfigMgr->GetOption<int32>("Dynamic.XP.Rate.60-67", 7);
 
            else if (player->getLevel() <= 79)
-            amount *= sConfigMgr->GetIntDefault("Dynamic.XP.Rate.70-79", 8);
+            amount *= sConfigMgr->GetOption<int32>("Dynamic.XP.Rate.70-79", 8);
 
         }
 
